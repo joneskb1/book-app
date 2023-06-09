0eecb33c146df24950b0cab7b55aa90c3e910474
hobbs9458 <hobbs9458@gmail.com>
2023-06-09 12:04:29 -0500

    changed build to dist

--- server/app.js
+++ server/app.js
@@ -47,9 +47,9 @@ mongoose.connect(DB).then(() => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static(path.join(__dirname, '/client/dist')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
   );
 }
 
