c0b6d8c7e80d836287f3f481ab04b418eb5b9e41
kayla <kayla3445@yahoo.com>
2024-02-01 11:05:10 -0600

    testing brevo email

--- server/utils/email.js
+++ server/utils/email.js
@@ -13,20 +13,20 @@ const html = fs.readFileSync(
 
 exports.sendMail = async (email, url) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.SEND_BLUE_HOST,
-    port: process.env.SEND_BLUE_PORT,
+    host: process.env.BREVO_EMAIL_HOST,
+    port: process.env.BREVO_EMAIL_PORT,
     auth: {
-      user: process.env.SEND_BLUE_EMAIL_USERNAME,
-      pass: process.env.SEND_BLUE_EMAIL_PASSWORD,
+      user: process.env.BREVO_EMAIL_USERNAME,
+      pass: process.env.BREVO_EMAIL_PASSWORD,
     },
-    from: process.env.SEND_BLUE_EMAIL_USERNAME,
+    from: process.env.BREVO_EMAIL_USERNAME,
   });
 
   let newHtml = html.replace('_URL_', url);
   // newHtml = newHtml.replace('_IMG_', dataUrl);
 
   const mailOptions = {
-    from: process.env.SEND_BLUE_EMAIL_USERNAME,
+    from: process.env.BREVO_EMAIL_USERNAME,
     to: email,
     subject: 'Reset Book Nook Password',
     html: newHtml,
